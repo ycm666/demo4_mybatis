87b9fe6f0c6f712c0b921be2f0b24977ad66443c
ycm111 <ycm666@naver.com>
2024-03-15 22:08:23 +0900

    properties 편집

--- src/main/java/com/example/demo4/Demo4Application.java
+++ src/main/java/com/example/demo4/Demo4Application.java
@@ -2,9 +2,16 @@ package com.example.demo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo4Application {
+public class Demo4Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Demo4Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo4Application.class, args);
